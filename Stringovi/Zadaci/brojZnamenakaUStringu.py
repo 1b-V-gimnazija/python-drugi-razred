eb19407e3a0ccaf2a6b6c279e98b9aef02f2d77e
Kontributor1 <105976474+Kontributor1@users.noreply.github.com>
2022-11-11 18:49:25 +0100

    Update brojZnamenakaUStringu.py

--- Stringovi/Zadaci/brojZnamenakaUStringu.py
+++ Stringovi/Zadaci/brojZnamenakaUStringu.py
@@ -6,8 +6,8 @@ numbers = 0
 # Petlja koja se vrti onoliko puta koliko je string dug, tako da možemo provjeriti svaki znak.
 for i in range(len(symbols)):
     # Koristeći metodu isnumeric() provjeravamo je li i-ti znak broj ili ne.
-    # (Metoda isnumberic() će vratiti True ako string (jedan simbol ili više njih) je broj.)
-    if symbols[i].isnumeric() == True:
+    # (Metoda isdigit() će vratiti True ako string (jedan simbol ili više njih) je broj.)
+    if symbols[i].isdigit() == True:
         # Dodajemo jedan naše brojaču.
         numbers += 1
 
